a63c87c9ef69716e6e8f2b2139f369933d059324
Disni Jayawickrama <it20147778@my.sliit.lk>
2023-10-18 14:58:09 +0530

    view of ticket- changes

--- frontend/src/components/Ticket/View.js
+++ frontend/src/components/Ticket/View.js
@@ -4,59 +4,100 @@ import { Link } from 'react-router-dom';
 
 const TicketManagement = () => {
   // Define sample ticket data
-  const ticketData = [
-    {
-      refID: '123D',
-      trainName: 'Express 123',
-      to: 'Moratuwa',
-      from: 'Gampaha',
-      ticketFare: '$400',
-      seatNo: '4A',
-      time: '19:00',
-      date: '10-11-23'
-    },
-    {
-      refID: '573E',
-      trainName: 'Econmy 123',
-      to: 'Galle',
-      from: 'Colombo',
-      ticketFare: '$1200',
-      seatNo: '32D',
-      time: '20:00',
-      date: '12-11-23'
-    },
-    {
-      refID: '124D',
-      trainName: 'Express 123',
-      to: 'Moratuwa',
-      from: 'Gampaha',
-      ticketFare: '$400',
-      seatNo: '12A',
-      time: '19:00',
-      date: '10-11-23'
-    },
-    {
-      refID: '345E',
-      trainName: 'Econmy 123',
-      to: 'Moratuwa',
-      from: 'Colombo',
-      ticketFare: '$100',
-      seatNo: '15C',
-      time: '10:00',
-      date: '02-11-23'
-    },
+  // const ticketData = [
+  //   {
+  //     refID: '123D',
+  //     trainName: 'Express 123',
+  //     to: 'Moratuwa',
+  //     from: 'Gampaha',
+  //     ticketFare: '$400',
+  //     seatNo: '4A',
+  //     time: '19:00',
+  //     date: '10-11-23'
+  //   },
+  //   {
+  //     refID: '573E',
+  //     trainName: 'Econmy 123',
+  //     to: 'Galle',
+  //     from: 'Colombo',
+  //     ticketFare: '$1200',
+  //     seatNo: '32D',
+  //     time: '20:00',
+  //     date: '12-11-23'
+  //   },
+  //   {
+  //     refID: '124D',
+  //     trainName: 'Express 123',
+  //     to: 'Moratuwa',
+  //     from: 'Gampaha',
+  //     ticketFare: '$400',
+  //     seatNo: '12A',
+  //     time: '19:00',
+  //     date: '10-11-23'
+  //   },
+  //   {
+  //     refID: '345E',
+  //     trainName: 'Econmy 123',
+  //     to: 'Moratuwa',
+  //     from: 'Colombo',
+  //     ticketFare: '$100',
+  //     seatNo: '15C',
+  //     time: '10:00',
+  //     date: '02-11-23'
+  //   },
     
 
-    // Add more train data as needed
-  ];
+  //   // Add more train data as needed
+  // ];
 
   // State to store the search term
   const [searchTerm, setSearchTerm] = useState('');
+  const [ticketData, setTicketData] = useState([]);
 
   // Event handler for search input
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
+
+    // Function to fetch the list of trains
+    const fetchTickets = () => {
+      fetch('https://localhost:7261/api/Train')
+        .then((response) => response.json())
+        .then((data) => setTicketData(data))
+        .catch((error) => console.error('Error fetching tickets:', error));
+    };
+
+  //Delete function
+  const handleDeleteTicket = (ticketId) => {
+    // Display a confirmation dialog to the user
+    const userConfirmed = window.confirm("Are you sure you want to delete this ticket?");
+  
+    if (userConfirmed) {
+      // User confirmed, proceed with deletion
+      fetch(`https://localhost:7261/api/Train/${ticketId}`, {
+        method: 'DELETE',
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log('Ticket deleted successfully');
+            alert('Ticket deleted successfully');
+            fetchTickets();
+          } else {
+            console.error('Error deleting Ticket:', response.status);
+            alert('Ticket deletion failed');
+          }
+        })
+        .catch((error) => {
+          console.error('Error deleting Ticket:', error);
+          alert('Ticket deletion failed');
+        });
+    }
+  };
+
+
+
+
+
 
   return (
 
@@ -115,8 +156,9 @@ const TicketManagement = () => {
                 <td>{ticket.time}</td>
                 <td>{ticket.date}</td>
                 <td> <Link to="/ticket-update"><button type="button" class="btn btn-warning"><i className="fas fa-edit"></i>&nbsp; Update</button></Link>
-                &nbsp; <Link to="/#"><button type="button" class="btn btn-danger"><i className ="far fa-trash-alt"> </i>&nbsp; Delete</button></Link></td>
-                
+                &nbsp; 
+                {/* <Link to="/#"><button type="button" class="btn btn-danger"><i className ="far fa-trash-alt"> </i>&nbsp; Delete</button></Link>*/}
+                <button type="button" className="btn btn-danger" onClick={() => handleDeleteTicket(ticket.id)}> <i className="far fa-trash-alt"></i>&nbsp; Delete Ticket </button></td>
                 
                 
               </tr>
