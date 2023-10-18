76e33214dd2c7fced64dc03e004fa675e82e38ad
vihaan1997 <vihaan1997@gmail.com>
2023-10-18 14:46:52 +0530

    Changes in backend.

--- WebService/Model/Reservations.cs
+++ WebService/Model/Reservations.cs
@@ -8,16 +8,43 @@ namespace TravelWebService.Model
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        [BsonElement("TravelerID")]
-        public string Traveler { get; set; } = string.Empty;
-
-        [BsonElement("TrainID")]
-        public string Train { get; set; } = string.Empty;
-
         [BsonElement("Date")]
-        public string Date { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
 
-        [BsonElement("AravingTime")]
-        public string AravingTime { get; set; } = string.Empty;
+        [BsonElement("Time")]
+        public string Time { get; set; } = string.Empty;
+
+        [BsonElement("StartingPoint")]
+        public string StartingPoint { get; set; } = string.Empty;
+
+        [BsonElement("EndingPoint")]
+        public string EndingPoint { get; set; } = string.Empty;
+
+        [BsonElement("AvailableTrains")]
+        public int AvailableTrains { get; set; }
+
+        [BsonElement("PassengerName")]
+        public string PassengerName { get; set; } = string.Empty;
+
+        [BsonElement("PassengerNIC")]
+        public string PassengerNIC { get; set; } = string.Empty;
+
+        [BsonElement("PassengerAge")]
+        public int PassengerAge { get; set; }
+
+        [BsonElement("Gender")]
+        public string Gender { get; set; } = string.Empty;
+
+        [BsonElement("DoB")]
+        public string DoB { get; set; } = string.Empty;
+
+        [BsonElement("ContactNo")]
+        public string ContactNo { get; set; } = string.Empty;
+
+        [BsonElement("Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [BsonElement("AdditionalMembers")]
+        public string AdditionalMembers { get; set; } = string.Empty;
     }
 }
